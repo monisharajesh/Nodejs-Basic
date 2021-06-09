be7fec7d897fc2cfd2c72d6560b108d97c9c5224
monisharajesh <monisharajesh078@gmail.com>
2021-06-09 12:10:18 +0530

    add my function calling

---
+++ basic.js
@@ -0,0 +1,15 @@
+console.log('I am Monisha Rajesh');
+/*const basic = function check(){
+    console.log('Now I am working');
+
+}*/
+
+/*const basic = function signin() {
+console.log('Enter the User name')
+console.log('Enter the password')
+}*/
+var basic = function view() {
+    console.log('Function calling is done')
+}
+
+module.exports = basic
---
+++ user.js
@@ -0,0 +1,4 @@
+const basic = require('./basic')
+console.log(basic('check'))
+console.log(basic('signin'))
+console.log(basic('view'))
